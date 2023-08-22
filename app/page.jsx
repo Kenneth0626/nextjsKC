7680b1796531e32a132d8d948a07b8d40c7bd043
kenneth <kenneth.c@agentsofvalue.com>
2023-08-22 14:46:39 +0800

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import HelloWorld from "@/components/hello";
+
+export default function Home() {
+    return (
+        <main className="flex"> 
+            <HelloWorld />
+        </main>
+    )
+}
---
+++ components/hello.jsx
@@ -0,0 +1,8 @@
+
+export default function HelloWorld () {
+    return (
+        <h1 className="mx-auto mt-[10rem] text-5xl">
+            Hello World!
+        </h1>
+    )
+}
